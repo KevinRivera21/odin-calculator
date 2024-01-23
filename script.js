be585f4355573f8b5231a03ffe348e1a92f0ceca
Kevin <kevinriverag2021@gmail.com>
2024-01-22 21:36:06 -0400

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,23 @@
+function add (firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+};
+
+console.log(add(10, 10));
+
+function subtract (firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+console.log(subtract(5, 10));
+
+function multiply (firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+console.log(multiply(10, 10));
+
+function divide (firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+console.log(divide(0, 10));
